ac7c1e9d657bf039090209412c90c96f77225fdc
Sebastián Mateo <sebastiangmateo@gmail.com>
2018-08-21 11:42:49 -0300

    #Refactor Merged NPC and Player into Character

--- Source/UnrealTestingGrounds/UnrealTestingGroundsGameMode.cpp
+++ Source/UnrealTestingGrounds/UnrealTestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AUnrealTestingGroundsGameMode::AUnrealTestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Behaviour/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
