1a035a23a52191c537cb3ab9e5b3afb0690f1f6f
Sebastián Mateo <sebastiangmateo@gmail.com>
2018-08-22 21:00:49 -0300

    #Refactor Moved the characters from BP to C++

--- Source/UnrealTestingGrounds/Weapons/Gun.cpp
+++ Source/UnrealTestingGrounds/Weapons/Gun.cpp
@@ -58,18 +58,19 @@ void AGun::OnFire()
 	}
 
 	// try and play the sound if specified
-	if (FireSound != NULL)
+	if (FireSound != nullptr)
 	{
 		UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
 	}
 
 	// try and play a firing animation if specified
-	if (FireAnimation != NULL)
+	if (FireAnimationFP != nullptr && AnimInstanceFP != nullptr)
 	{
-		// Get the animation object for the arms mesh
-		if (AnimInstance != NULL)
-		{
-			AnimInstance->Montage_Play(FireAnimation, 1.f);
-		}
+		AnimInstanceFP->Montage_Play(FireAnimationFP, 1.f);
+	}
+
+	if (FireAnimationTP != nullptr && AnimInstanceTP != nullptr)
+	{
+		AnimInstanceTP->Montage_Play(FireAnimationTP, 1.f);
 	}
 }
